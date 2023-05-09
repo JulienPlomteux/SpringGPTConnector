492f79bb5d8a87f78fb72fdc11e8e019fce8fc4b
Mrlafontaine <archer19@hotmail.ca>
2023-05-08 21:20:14 -0400

    Continue...

---
+++ src/main/java/com/plomteux/gptconnector/controller/ChatControllerApiImpl.java
@@ -0,0 +1,22 @@
+package com.plomteux.gptconnector.controller;
+
+import com.plomteux.gptconnector.model.ChatGPTRequest;
+import com.plomteux.gptconnector.model.ChatGPTResponse;
+import com.plomteux.gptconnector.service.ChatService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class ChatControllerApiImpl implements ChatControllerApi {
+    private ChatService chatService;
+    private static final Logger logger = LoggerFactory.getLogger(ChatControllerApiImpl.class);
+
+    public ResponseEntity<ChatGPTResponse> sendMessage(ChatGPTRequest chatGPTRequest) {
+        logger.debug("Received request: {}", chatGPTRequest);
+        return chatService.generateText(chatGPTRequest);
+    }
+}
---
+++ src/test/java/com/plomteux/gptconnector/GptConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plomteux.gptconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
