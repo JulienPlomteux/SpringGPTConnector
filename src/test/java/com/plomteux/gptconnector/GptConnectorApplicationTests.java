404902a28ab15bbdc70bb95d969b9f3a574d0328
Mrlafontaine <archer19@hotmail.ca>
2023-05-23 21:12:28 -0400

    Continue...

--- src/test/java/com/plomteux/gptconnector/GptConnectorApplicationTests.java
+++ src/test/java/com/plomteux/gptconnector/GptConnectorApplicationTests.java
@@ -2,8 +2,10 @@ package com.plomteux.gptconnector;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GptConnectorApplicationTests {
 
     @Test
